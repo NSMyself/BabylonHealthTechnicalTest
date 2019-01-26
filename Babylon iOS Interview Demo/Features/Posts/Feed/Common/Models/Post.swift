20b3da07fc2abbc26f283963d6d603e4cc49ee1c
João Pereira <knight4@gmail.com>
2019-01-26 19:07:48 +0000

    Alter Post mode to comply with Realm's requirements

--- Babylon iOS Interview Demo/Features/Posts/Feed/Common/Models/Post.swift
+++ Babylon iOS Interview Demo/Features/Posts/Feed/Common/Models/Post.swift
@@ -8,9 +8,32 @@
 
 import Foundation
 
-public struct Post: Codable, Hashable {
-    let id: Int
-    let userId: Int
-    let title: String
-    let body: String
+public class Post: Codable {
+    @objc dynamic var id: Int = 0
+    @objc dynamic var userId: Int = 0
+    @objc dynamic var title: String = ""
+    @objc dynamic var body: String = ""
+    
+    init(id: Int, userId: Int, title: String, body: String) {
+        self.id = id
+        self.userId = userId
+        self.title = title
+        self.body = body
+    }
+}
+
+extension Post: Hashable {
+    public static func == (lhs: Post, rhs: Post) -> Bool {
+        return lhs.id == rhs.id
+            && lhs.userId == rhs.userId
+            && lhs.title == rhs.title
+            && lhs.body == rhs.body
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(userId)
+        hasher.combine(title)
+        hasher.combine(body)
+    }
 }
